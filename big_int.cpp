70ae638fe6b332d040aa2a25f8066d7367e2fc13
Hocine <ocineh@hotmail.com>
2022-05-26 08:21:44 +0200

    Overloads the addition operator for the BigInt class

--- big_int.cpp
+++ big_int.cpp
@@ -62,7 +62,7 @@ int cmp_abs(const BigInt &a, const BigInt &b) {
 	auto j = b.m_digits.begin();
 	for(; i != end; ++i, ++j)
 		if(*i != *j)
-			return *i < *j ? 1 : -1;
+			return *i < *j ? -1 : 1;
 	return 0;
 }
 
@@ -92,4 +92,63 @@ bool operator<=(const BigInt &lhs, const BigInt &rhs) {
 
 bool operator>=(const BigInt &lhs, const BigInt &rhs) {
 	return cmp(lhs, rhs) >= 0;
+}
+
+void BigInt::strip() {
+	while(!m_digits.empty() && m_digits.front() == 0)
+		m_digits.pop_front();
+	if(m_digits.empty()) m_digits.push_front(0);
+}
+
+BigInt operator+(const BigInt &lhs, const BigInt &rhs) {
+	BigInt result;
+
+	// Determine the sign of the result
+	int cmp = cmp_abs(lhs, rhs);
+	bool same_sign = lhs.m_negative == rhs.m_negative;
+	if(same_sign) result.m_negative = lhs.m_negative;
+	else {
+		if(cmp == -1) result.m_negative = rhs.m_negative;
+		else if(cmp == 1) result.m_negative = lhs.m_negative;
+		else { // x + -x = 0
+			result.m_negative = false;
+			result.m_digits.push_front(0);
+			return result;
+		}
+	}
+
+	// Calculate the result
+	std::list<unsigned char>::const_reverse_iterator i, j, i_end, j_end;
+	if(same_sign || cmp == 1) {
+		i = lhs.m_digits.rbegin(), i_end = lhs.m_digits.rend();
+		j = rhs.m_digits.rbegin(), j_end = rhs.m_digits.rend();
+	} else { // cmp == -1
+		i = rhs.m_digits.rbegin(), i_end = rhs.m_digits.rend();
+		j = lhs.m_digits.rbegin(), j_end = lhs.m_digits.rend();
+	}
+
+	int carry = 0, sum;
+	for(; i != i_end && j != j_end; ++i, ++j) {
+		if(same_sign) sum = *i + *j + carry;
+		else if(*i == *j) sum = carry < 0 ? -(20 + carry) : carry;
+		else if(*i < *j) sum = -10 - (10 - (*j - *i - carry));
+		else sum = *i - *j + carry;
+
+		carry = sum / 10;
+		result.m_digits.push_front(abs(sum % 10));
+	}
+
+	auto it = i == i_end ? j : i;
+	auto end = i == i_end ? j_end : i_end;
+	for(; it != end; ++it) {
+		if((-carry) > *it) sum = -10 - (10 - (*it + -carry));
+		else sum = *it + carry;
+
+		carry = sum / 10;
+		result.m_digits.push_front(abs(sum % 10));
+	}
+
+	if(carry != 0) result.m_digits.push_front(abs(carry));
+	result.strip();
+	return result;
 }
