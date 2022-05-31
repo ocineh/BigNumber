8f7900f52493bf295b2b2f2479a8ec7ee59a290b
Hocine <ocineh@hotmail.com>
2022-05-31 11:42:38 +0200

    Overloads the increment and decrement operators for the BigFloat class

--- src/big_float_unary.cpp
+++ src/big_float_unary.cpp
@@ -5,3 +5,23 @@ BigFloat operator-(BigFloat const &i) {
 	result.m_negative = !result.m_negative;
 	return result;
 }
+
+BigFloat &operator++(BigFloat &n) {
+	return n += BigFloat{ 1 };
+}
+
+BigFloat &operator--(BigFloat &n) {
+	return n -= BigFloat{ 1 };
+}
+
+BigFloat operator++(BigFloat &n, int) {
+	BigFloat result{ n };
+	n += BigFloat{ 1 };
+	return result;
+}
+
+BigFloat operator--(BigFloat &n, int) {
+	BigFloat result{ n };
+	n -= BigFloat{ 1 };
+	return result;
+}
